82d49bcb7d9173a9eb60bf89e81da61ad0815a35
omarrashid <omarrashid14@hotmail.com>
2014-11-05 17:14:40 +0000

    Hi this is the student class dawg

---
+++ src/test/Student.java
@@ -0,0 +1,20 @@
+package test;
+
+public class Student {
+	private String name;
+	private int age;
+	private String module;
+	
+	public Student (String name, int age, String module){
+		this.name = name;
+		this.age = age;
+		this.module = module;
+		
+		
+	}
+	
+	public void printStudent(){
+		System.out.println("the name of the Student is"+name );
+	}
+
+}
